7e48debe2d079c0596042617966cc202427ce856
Chris McAnally <chris@adadevelopersacademy.org>
2021-06-16 15:29:51 -0700

    finished without extra promise

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const axios = require('axios');
+require('dotenv').config();
 
 const LOCATIONIQ_KEY = process.env['API_KEY'];
 
-const findLatitudeAndLongitude = (query) => {
+const findLatitudeAndLongitude = (location) => {
   let latitude, longitude;
   axios.get('https://us1.locationiq.com/v1/search.php',
   {
     params: {
       key: LOCATIONIQ_KEY,
-      q: 'Seattle, Washington, USA',
+      q: location,
       format: 'json'
     }
   })
   .then( (response) => {
     latitude = response.data[0].lat;
     longitude = response.data[0].lon;
-    console.log('success in findLatitudeAndLongitude!', latitude, longitude);
+    console.log(`${location} is at (${latitude}, ${longitude})`);
   })
   .catch( (error) => {
     console.log('error in findLatitudeAndLongitude!');
@@ -27,27 +28,34 @@ const findLatitudeAndLongitude = (query) => {
   }
 }
 
-const findLocation = (latitude, longitude) => {
-  axios.get('https://us1.locationiq.com/v1/reverse.php',
-  {
-    params: {
-      key: LOCATIONIQ_KEY,
-      format: 'json',
-      lat: latitude,
-      lon: longitude
+// getLocations
+// Go through the array of locations
+// and use the API to find each location
+// Make each API call after 1000 ms
+const getLocations = () => {
+  const locations = [
+    "Great Wall of China",
+    "Petra",
+    "Colosseum",
+    "Chichen Itza",
+    "Machu Picchu",
+    "Taj Mahal",
+    "Christ the Redeemer",
+  ];
+
+  // Every 1000 ms call findLatitudeAndLongitude
+  // After finishing the array, end the interval.
+  let currentIndex = 0;
+  const intervalObj = setInterval(() => {
+    const coordinates = findLatitudeAndLongitude(locations[currentIndex]);
+
+    currentIndex += 1;
+
+    // If we've found all the locations stop the interval
+    if (currentIndex >= locations.length) {
+      clearInterval(intervalObj);
     }
-  })
-  .then( (response) => {
-    console.log('success in findLocation!', response.data);
-    return response.data;
-  })
-  .catch( (error) => {
-    console.log('error in findLocation!');
-  });
+  }, 1000)
 }
 
-const seattleCoordinates = findLatitudeAndLongitude('Seattle, Washington, USA');
-
-const locations = findLocation(seattleCoordinates.seattleLat, seattleCoordinates.seattleLon);
-
-console.log(locations);
+getLocations();
